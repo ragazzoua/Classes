5c8dca5205d6d43ef7735b42e3e59fb058c3c768
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-08-30 17:30:16 +0300

    classes

---
+++ src/com/company/Car.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * created by s.a.miroshnychenko 8/28/2018
+ */
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carerra") || validModel.equals("camarro")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car porshe = new Car();
+        Car holden = new Car();
+
+        porshe.setModel("Carerra");
+        System.out.println("Model is " + porshe.getModel());
+    }
+}
